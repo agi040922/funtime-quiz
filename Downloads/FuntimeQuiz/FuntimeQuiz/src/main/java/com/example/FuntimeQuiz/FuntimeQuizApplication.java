a5767ce643ac4c706829ece741cf6325c9b8a997
agi040922 <166208125+agi040922@users.noreply.github.com>
2025-02-23 11:50:44 +0900

    프로젝트 초기 설정 및 Gradle 의존성 추가

---
+++ Downloads/FuntimeQuiz/FuntimeQuiz/src/main/java/com/example/FuntimeQuiz/FuntimeQuizApplication.java
@@ -0,0 +1,13 @@
+package com.example.FuntimeQuiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuntimeQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuntimeQuizApplication.class, args);
+	}
+
+}
---
+++ Downloads/FuntimeQuiz/FuntimeQuiz/src/test/java/com/example/FuntimeQuiz/FuntimeQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FuntimeQuiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuntimeQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
